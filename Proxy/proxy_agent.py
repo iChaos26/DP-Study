c151fc08f7ee51878e81b68c12073e1a9f5a2ae9
iChaos26 <joony-26@hotmail.com>
2020-07-28 17:04:15 -0300

    Proxy example

---
+++ Proxy/proxy_agent.py
@@ -0,0 +1,28 @@
+class Actor(object):
+    def __init__(self):
+        self.isBusy = False
+
+    def occupied(self):
+        self.isBusy = True
+        print(type(self).__name__, "is occupied with current movie") 
+
+    def available(self):
+        self.isBusy = False
+        print(type(self).__name__, "is free for the movie ")
+
+    def getStatus(self):
+        return self.isBusy
+#Proxy
+class Agent(object):
+    def __init__(self):
+        self.principal = None
+
+    def work(self):
+        self.actor = Actor()
+        if self.actor.getStatus():
+            self.actor.occupied()
+        else:
+            self.actor.available()
+
+if __name__ == '__main__':
+    Agent().work()
