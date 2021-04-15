e145d8cbea652c80adea5fd356a682968f5516ff
João Gabriel <joony-26@hotmail.com>
2021-04-15 09:52:30 -0300

    updating factory, need to implement principal hooks
    - Livy Operator
    - K8s operator
    - emr operator

--- Factory/DAGFactory.py
+++ Factory/DAGFactory.py
@@ -1,54 +1,90 @@
-import os
-import sys
-import traceback
-
 from airflow import DAG
-from importlib import import_module
-
-# initialize airflow base dir
-AIRFLOW_BASE =  os.path.dirname(
-                os.path.dirname(
-                os.path.abspath(__file__)))
-sys.path.append(AIRFLOW_BASE)
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
 
 
-class DAGFactory(object):
+class DAGFactory:
     """
-    Dag factory: collect dags from all sub folders under ./airflow/projects
+    Class that provides useful method to build an Airflow DAG
     """
 
-    def __init__(self):
-        self.project_base = os.path.join(
-                                  AIRFLOW_BASE 
-                                , 'projects' )
+    @classmethod
+    def create_dag(cls, dagname, default_args={}, catchup=False, concurrency=5, cron=None):
+        """
+        params:
+            dagname(str): the name of the dag
+            default_args(dict): a dict with the specific keys you want to edit from the original DEFAULT_ARGS
+            catchup(bool): Perform scheduler catchup (or only run latest)? Defaults to True
+            concurrency(int): the number of task instances allowed to run concurrently
+            cron(str): the cron expression or the schedule
+        returns:
+            DAG object
+        """
+        DEFAULT_ARGS = {
+            'owner': 'Data Engineer',
+            'depends_on_past': False,
+            'start_date': datetime(2021, 1, 1),
+            'email': ['data_engineers@company.com'],
+            'email_on_failure': True,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),
+        }
 
-    @property
-    def projects(self):
-        return list(
-                    filter( lambda x : x not in ['__pycache__', '.idea'] ,
-                            [dirs for _, dirs, _ in os.walk(self.project_base) ][0]) )
+        DEFAULT_ARGS.update(default_args)
+        dagargs = {
+            'default_args': DEFAULT_ARGS,
+            'schedule_interval': cron,
+            'catchup': catchup,
+            'concurrency': concurrency
+        }
 
-    def get_modules_from_all_projects(self):
-        #get all modules from the project and import itself 
-        for project_name in self.projects:
-            try:
-                module_name = "projects.{}.DAG".format(project_name)
-                prj_mod = import_module(module_name)
-                no_of_project_dags = 1
+        dag = DAG(dagname, **dagargs)
+        return dag
 
-                for dag in prj_mod.DAGS:
-                    var_name = '{}-{}'.format(project_name, no_of_project_dags)
+    @classmethod
+    def add_tasks_to_dag(cls, dag, tasks):
+        """
+        Adds tasks to DAG object, sets upstream for each task.
+        params:
+            dag(DAG)
+            tasks(dict): dictionary in which each key is a callback. The value of that key is the task's dependencies.
+            If a task has no dependencies (it's the first task), set an empty list [] as the value.
+            IMPORTANT: all tasks have to be there even if they don't have dependencies
+        returns:
+            dag(DAG) with tasks
+        """
+        with dag as dag:
+            aux_dict = {}
 
-                    # create unique name for project in global namespace
-                    globals()[var_name] = dag
+            # create task objects and store them in a dictionary of "func name": task
+            for func in tasks:
+                task_id = func.__name__
+                task = PythonOperator(
+                    task_id=task_id,
+                    python_callable=func,
+                    dag=dag
+                )
+                aux_dict[task_id] = task
 
-                    no_of_project_dags += 1
-            except Exception as e:
-                print( traceback.format_exc() )
+            # for each task, set up the tasks predecessors
+            for func, dependencies in tasks.items():
+                task_id = func.__name__
+                # does not have dependencies? then it's the first task
+                for dep in dependencies:
+                    aux_dict[dep.__name__] >> aux_dict[task_id]
 
+        return dag
 
-
-dags = DAGFactory()
-print( dags.projects )
-
-dags.get_modules_from_all_projects()
+    @classmethod
+    def get_airflow_dag(cls, dagname, tasks, default_args={}, catchup=False, concurrency=5, cron=None):
+        """
+        The actual method that has to be called by a DAG file to get the dag.
+        params:
+            idem as create_dag + add_tasks_to_dag
+        returns:
+            DAG object
+        """
+        dag = cls.create_dag(dagname, default_args=default_args, catchup=catchup, concurrency=concurrency, cron=cron)
+        dag = cls.add_tasks_to_dag(dag, tasks)
+        return dag
